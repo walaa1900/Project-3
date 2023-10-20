a9e17088cb22af8846f33a02a2619032896234ae
rosamp1 <132237292+rosamp1@users.noreply.github.com>
2023-10-19 20:21:51 -0500

    apis complete

--- app.py
+++ app.py
@@ -35,6 +35,101 @@ def get_insurance_data():
 
     return jsonify(formatted_results)
 
+@app.route("/insurance_data_region")
+def get_insurance_data_region():
+    # Establish a database connection
+    connection = engine.connect()
+
+    # Define your SQL query to select data from the 'insurance csv for sql' table
+    sql_query = "SELECT region, charges FROM 'insurance csv for sql'"
+
+    # Execute the query and fetch the results
+    results = connection.execute(sql_query).fetchall()
+
+    # Close the database connection
+    connection.close()
+
+    # Format the results as needed (e.g., convert to a list of dictionaries)
+    formatted_results = [dict(result) for result in results]
+
+    return jsonify(formatted_results)
+
+@app.route("/insurance_data_age")
+def get_insurance_data_age():
+    # Establish a database connection
+    connection = engine.connect()
+
+    # Define your SQL query to select data from the 'insurance csv for sql' table
+    sql_query = "SELECT age, charges FROM 'insurance csv for sql'"
+
+    # Execute the query and fetch the results
+    results = connection.execute(sql_query).fetchall()
+
+    # Close the database connection
+    connection.close()
+
+    # Format the results as needed (e.g., convert to a list of dictionaries)
+    formatted_results = [dict(result) for result in results]
+
+    return jsonify(formatted_results)
+
+@app.route("/insurance_data_gender")
+def get_insurance_data_gender():
+    # Establish a database connection
+    connection = engine.connect()
+
+    # Define your SQL query to select data from the 'insurance csv for sql' table
+    sql_query = "SELECT sex, charges FROM 'insurance csv for sql'"
+
+    # Execute the query and fetch the results
+    results = connection.execute(sql_query).fetchall()
+
+    # Close the database connection
+    connection.close()
+
+    # Format the results as needed (e.g., convert to a list of dictionaries)
+    formatted_results = [dict(result) for result in results]
+
+    return jsonify(formatted_results)
+
+@app.route("/insurance_data_bmi")
+def get_insurance_data_bmi():
+    # Establish a database connection
+    connection = engine.connect()
+
+    # Define your SQL query to select data from the 'insurance csv for sql' table
+    sql_query = "SELECT bmi, charges FROM 'insurance csv for sql'"
+
+    # Execute the query and fetch the results
+    results = connection.execute(sql_query).fetchall()
+
+    # Close the database connection
+    connection.close()
+
+    # Format the results as needed (e.g., convert to a list of dictionaries)
+    formatted_results = [dict(result) for result in results]
+
+    return jsonify(formatted_results)
+
+@app.route("/insurance_data_smoker")
+def get_insurance_data_smoker():
+    # Establish a database connection
+    connection = engine.connect()
+
+    # Define your SQL query to select data from the 'insurance csv for sql' table
+    sql_query = "SELECT smoker, charges FROM 'insurance csv for sql'"
+
+    # Execute the query and fetch the results
+    results = connection.execute(sql_query).fetchall()
+
+    # Close the database connection
+    connection.close()
+
+    # Format the results as needed (e.g., convert to a list of dictionaries)
+    formatted_results = [dict(result) for result in results]
+
+    return jsonify(formatted_results)
+
 if __name__ == "__main__":
     app.run(debug=True)
 
